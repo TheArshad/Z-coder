60fa615ad772391a748e0450c060e688468fdf48
avinas29 <btiati827@gmail.com>
2025-06-07 19:41:14 +0530

    Update Calendar.js

--- frontend/src/components/Calendar/Calendar.js
+++ frontend/src/components/Calendar/Calendar.js
@@ -3,6 +3,7 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import './Calendar.css';
 
+
 function EventCalendar({ events }) {
     const [selectedDate, setSelectedDate] = useState(new Date());
 
